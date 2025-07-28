5a43c43a2c71279d1a08ae2a57ff241a492066c1
moega <moegamattauriqosman@gmail.com>
2025-07-28 11:20:51 +0200

    fixed imports

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../css/index.css'
-import App from './App.jsx'
-import CreateAnAlert from "./CreateAnAlert.jsx";
+import '../css/Login.css'
+import Login from "./Login.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-      <CreateAnAlert/>
+    
+      <Login/>
   </StrictMode>,
 )
