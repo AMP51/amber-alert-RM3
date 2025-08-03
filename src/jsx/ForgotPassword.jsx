79312b62bbff0db5e0619cc01970448ffce31ac6
moega <moegamattauriqosman@gmail.com>
2025-08-03 21:28:51 +0200

    lrfr pages

--- src/jsx/ForgotPassword.jsx
+++ src/jsx/ForgotPassword.jsx
@@ -1,45 +1,56 @@
 import '../components/lrfLayout.css';
 import logo from '../assets/logo.jpg';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function ForgotPassword() {
-    return (
-        <div className="lrf-container">
-            <div className="lrf-form">
-                <img src={logo} alt="Logo" className="logo" />
-                <h1 className="login">Forgot Password <br></br> Your Password ?</h1>
+  const [resetSuccess, setResetSuccess] = useState(false);
 
-                <div className="form">
-                    <div className="input-field">
-                        <label className="input-label">Email</label>
-                        <input
-                            type="email"
-                            className="input-control"
-                            defaultValue=""
-                        />
-                    </div>
+  const handleReset = () => {
+    setResetSuccess(true);
+  };
 
-                    <div className="input-field">
-                        <label className="input-label">Phone Number</label>
-                        <input
-                            type="tel"
-                            className="input-control"
-                            defaultValue=""
-                        />
-                    </div>
-                </div>
+  return (
+    <div className="lrf-container">
+      <div className="lrf-form">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1 className="login">
+          Forgot Password <br /> Your Password ?
+        </h1>
 
-                <button className="lrf-button">
-                    <span className="button-text">Reset Password</span>
-                </button>
+        {resetSuccess && (
+          <div style={{ color: 'green', marginBottom: '15px' }}>
+            Password reset successfully!
+          </div>
+        )}
 
-                <div className="links">
-                    <Link to="/login" className="link">Already have an account? Click here</Link>
-                    <Link to="/register" className="link">Don't have an account? Click here</Link>
-                </div>
-            </div>
+        <div className="form">
+          <div className="input-field">
+            <label className="input-label">Email</label>
+            <input type="email" className="input-control" defaultValue="" />
+          </div>
+
+          <div className="input-field">
+            <label className="input-label">Phone Number</label>
+            <input type="tel" className="input-control" defaultValue="" />
+          </div>
         </div>
-    );
+
+        <button type="button" className="lrf-button" onClick={handleReset}>
+          <span className="button-text">Reset Password</span>
+        </button>
+
+        <div className="links">
+          <Link to="/login" className="link">
+            Already have an account? Click here
+          </Link>
+          <Link to="/register" className="link">
+            Don't have an account? Click here
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ForgotPassword;
